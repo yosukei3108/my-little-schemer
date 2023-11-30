6547588a3164eb9a30c1ca7bfb18bc406a657a9f
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-11-30 09:15:37 +0900

    add 39, 42, 44

--- 1.scm
+++ 1.scm
@@ -8,5 +8,19 @@
 ; 6
 (print (atom? (quote (atom)))) ;#f
 
-;23
-(print (car ())) ; *** ERROR: pair required, but got ()
+; 23
+; (print (car ())) ; *** ERROR: pair required, but got ()
+
+; 39
+(print
+  (cons
+    (quote
+      ((help) this))
+    (quote
+      (is very ((hard) to learn))))) ; (((help) this) is very ((hard) to learn))
+
+; 42
+(print (cons (quote a) ())) ; (a)
+
+; 44
+(print (cons (quote a) (quote b))) ; (a . b) ; 脚注 5
