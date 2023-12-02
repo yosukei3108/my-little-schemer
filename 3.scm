8053e7f4c25937f1d71fed6327525808f90e42ce
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-02 15:00:27 +0900

    fix indent

--- 3.scm
+++ 3.scm
@@ -14,7 +14,7 @@
       ((null? lat) (quote ()))
       ((eq? (car lat) a) (cdr lat))
       (else (cons (car lat)
-        (rember a (cdr lat)))))))
+              (rember a (cdr lat)))))))
 
 ; 17
 (print (rember (quote bacon) (quote (bacon lettuce and tomato))))
