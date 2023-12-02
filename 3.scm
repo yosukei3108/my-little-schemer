c58f086f95cf6d37fca1a1b602cecd65774b7c3d
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-02 14:24:31 +0900

    update in 44

--- 3.scm
+++ 3.scm
@@ -4,8 +4,8 @@
       ((null? lat) (quote ()))
       (else (cond
               ((eq? (car lat) a) (cdr lat))
-              (else (rember a (cdr lat))))))))
-
+              (else (cons (car lat)
+                (rember a (cdr lat)))))))))
 ; 17
 (print (rember (quote bacon) (quote (bacon lettuce and tomato))))
 ; (lettuce and tomato)
@@ -13,3 +13,5 @@
 ; 27
 (print (rember (quote and) (quote (bacon lettuce and tomato))))
 ; (tomato)
+; with 44
+; (bacon lettuce tomato)
