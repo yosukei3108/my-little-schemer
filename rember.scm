75075bb9cb6440eaab1bf5c53f8b307655b7234e
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-02 14:21:53 +0900

    update rember with cons

--- rember.scm
+++ rember.scm
@@ -1,7 +1,17 @@
+; (define rember
+;   (lambda (a lat)
+;     (cond
+;       ((null? lat) (quote ()))
+;       (else (cond
+;               ((eq? (car lat) a) (cdr lat))
+;               (else (rember a (cdr lat))))))))
+
+; 3-44
 (define rember
   (lambda (a lat)
     (cond
       ((null? lat) (quote ()))
       (else (cond
               ((eq? (car lat) a) (cdr lat))
-              (else (rember a (cdr lat))))))))
+              (else (cons (car lat)
+                (rember a (cdr lat)))))))))
