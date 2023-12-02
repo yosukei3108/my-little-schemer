6b61a494546e494e311b1af5aa689de8840dd693
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-02 14:56:33 +0900

    update rember in 70

--- rember.scm
+++ rember.scm
@@ -7,11 +7,20 @@
 ;               (else (rember a (cdr lat))))))))
 
 ; 3-44
+; (define rember
+;   (lambda (a lat)
+;     (cond
+;       ((null? lat) (quote ()))
+;       (else (cond
+;               ((eq? (car lat) a) (cdr lat))
+;               (else (cons (car lat)
+;                 (rember a (cdr lat)))))))))
+
+; 3-70
 (define rember
   (lambda (a lat)
     (cond
       ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) a) (cdr lat))
-              (else (cons (car lat)
-                (rember a (cdr lat)))))))))
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+        (rember a (cdr lat)))))))
