4366b18ca02b29bc60c7b24d0c742dc3a085f2cc
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-09 13:58:03 +0900

    add 6, 8

--- 4.scm
+++ 4.scm
@@ -21,3 +21,21 @@
 (print
   (atom? 3.1415))
 ; #t
+
+; 6
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(print
+  (add1 67))
+; 68
+
+; 8
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(print
+  (sub1 5))
+; 4
