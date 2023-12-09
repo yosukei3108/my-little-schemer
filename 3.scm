6021824eddde732e1b6ccd3923a3249244b03ee1
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2023-12-09 12:52:11 +0900

    add 148

--- 3.scm
+++ 3.scm
@@ -156,3 +156,21 @@
 ; (vanilla ice cream with chocolate topping)
 ; 146
 ; (vanilla ice cream with chocolate topping)
+
+; 148
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+        (cond
+          ((eq? (car lat) a)
+            (multirember a (cdr lat)))
+          (else (cons (car lat)
+                  (multirember a (cdr lat)))))))))
+
+(print
+  (multirember
+    (quote cup)
+    (quote (coffee cup tea cup and hick cup))))
+; (coffee tea and hick)
